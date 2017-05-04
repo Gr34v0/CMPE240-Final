1fc1ebf4178882e35732bb778db7853950441a96
Adam Audycki <apa7395@rit.edu>
2017-05-04 14:44:22 -0400

    Need to figure out the DB schema that we want, and what is returned from the port.

--- Host/socket.php
+++ Host/socket.php
@@ -32,7 +32,32 @@ function writeToFile($strFilename, $strText) {
 }
 
 function saveToDB($strText){
-//Connect to DB
-//Create sql querry
-//execute querry
+    //Connect to DB
+    //Create sql querry
+    //execute querry
+
+    $servername = "localhost";
+    $username = "username";
+    $password = "cmpe240";
+
+    //Create connection
+    $conn = new mysqli($servername, $username, $password);
+
+    //Check connection
+    if($conn->connect_error){
+        die("Connection failed: " . $conn->connect_error);
+    } 
+    echo "Connected successfully";
+    
+    $sql = "INSERT INTO data (stuff) VALUES ('STUFF')";
+    
+    if($conn->query($sql) === TRUE){
+        echo "New record created successfully";
+    } 
+    else{
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+?>
 }    

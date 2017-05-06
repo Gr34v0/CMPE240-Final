f556f9547a222b3ced30c6b4702b1ae7f077ad26
Adam <apa7395@rit.edu>
2017-05-06 02:26:46 +0000

    This connects with my database and I believe it sets up the socket listener correctly. Will need to test it on laptop.

--- Host/socket.php
+++ Host/socket.php
@@ -1,24 +1,22 @@
 <?php
 
-// Server IP address. Will need to get own IP Address.
-$address = "xx.xxx.xxx.xxx";
 // Port to listen. Find out what port to use.
-$port = 80;
+$port = 1234;
 
-$mysock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-
-socket_bind($mysock,$address, $port) or die('Could not bind to address'); 
-socket_listen($mysock, 5);
-$client = socket_accept($mysock);
+$sock = socket_create_listen ($port);
+$client = socket_accept($sock);
 
 // read 1024 bytes from client
 $input = socket_read($client, 1024);
+
+echo "FROM SOCKET: " . $input . "<br>";
 
 //Will save data into DB.
 saveToDB($input);
 
 socket_close($client);
-socket_close($mysock);
+socket_close($sock);
+echo $input;
 
 //Will replace this with writing data into database.
 function writeToFile($strFilename, $strText) { 
@@ -26,8 +24,9 @@ function writeToFile($strFilename, $strText) {
           $contents = fwrite($fp, $strText); 
           fclose($fp); 
           return true; 
-    } else { 
-      return false; 
+    } 
+    else { 
+	return false; 
     } 
 }
 
@@ -37,7 +36,7 @@ function saveToDB($strText){
     //execute querry
 
     $servername = "localhost";
-    $username = "username";
+    $username = "root";
     $password = "cmpe240";
 
     //Create connection
@@ -47,6 +46,7 @@ function saveToDB($strText){
     if($conn->connect_error){
         die("Connection failed: " . $conn->connect_error);
     } 
+    
     echo "Connected successfully";
     
     $sql = "INSERT INTO data (stuff) VALUES ('STUFF')";
@@ -59,5 +59,6 @@ function saveToDB($strText){
     }
 
     $conn->close();
+}
 ?>
-}    
+   

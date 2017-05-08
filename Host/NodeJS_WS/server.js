e0d7526f9cc8db982cee1d34cfb2ddb7cf5f6e03
Adam Audycki <apa7395@rit.edu>
2017-05-08 14:43:49 -0400

    Changed some formatting and added some select statements.

--- Host/NodeJS_WS/server.js
+++ Host/NodeJS_WS/server.js
@@ -1,4 +1,3 @@
-
 var http = require('http');
 var express = require('express');
 var WSS = require('ws').Server;
@@ -9,49 +8,69 @@ server.listen(8080, '127.0.0.1');
 
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'cmpe240',
-  database : 'cmpe'
+    host: 'localhost',
+    user: 'root',
+    password: 'cmpe240',
+    database: 'cmpe'
 });
 
-var wss = new WSS({ port: 8081 });
-wss.on('connection', function(socket) {
-  console.log('Opened Connection ');
+var wss = new WSS({port: 8081});
+//Does some stuff when connection is established.
+wss.on('connection', function (socket) {
+    console.log('Opened Connection ');
 
-  var json = JSON.stringify({ message: 'Gotcha' });
-  socket.send(json);
-  console.log('Sent: ' + json);
-
-  socket.on('message', function(message) {
-    console.log('Received: ' + message);
-	writeToDB(message);
-
-  });
-
-  socket.on('close', function() {
-    console.log('Closed Connection ');
-  });
-
-});
-
-var broadcast = function() {
-  var json = JSON.stringify({
-    message: 'Hello hello!'
-  });
-
-  wss.clients.forEach(function each(client) {
-    client.send(json);
+    var json = JSON.stringify({message: 'Gotcha'});
+    socket.send(json);
     console.log('Sent: ' + json);
-  });
+
+    socket.on('message', function (message) {
+        console.log('Received: ' + message);
+        writeToDB(message);
+
+    });
+
+    socket.on('close', function () {
+        console.log('Closed Connection ');
+    });
+
+});
+
+//function that will send messages to the client side.
+var broadcast = function () {
+    var json = JSON.stringify({
+        message: 'Hello!'
+    });
+
+    wss.clients.forEach(function each(client) {
+        client.send(json);
+        console.log('Sent: ' + json);
+    });
 }
 setInterval(broadcast, 3000);
 
-function writeToDB(msg){
-	connection.query( "INSERT INTO data (data) VALUES ('" +msg + "')", function(err, rows, fields) {
-	  if (!err)
-	    console.log('The solution is: ', rows);
-	  else
-	    console.log('Error while performing Query.');
-	});
+//The function will write the message into the database.
+function writeToDB(msg) {
+    connection.query("INSERT INTO data (data) VALUES ('" + msg + "')", function (err, rows, fields) {
+        if (!err) {
+            console.log('The solution is: ', rows);
+        }
+        else {
+            console.log('Error while performing Query.');
+        }
+    });
+}
+
+function getAllDB(){
+    connection.query("SELECT * FROM data", function (err, rows, fields) {
+        if (!err) {
+            rows = JSON.stringify(rows);
+            wss.clients.forEach(function each(client) {
+                client.send(rows);
+                console.log('Sent: ' + rows);
+            });
+        }
+        else {
+            console.log('Error while performing Query.');
+        }
+    });
 }

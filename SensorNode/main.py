a59b8af570fca48c7b5d3323db08fe22bf90c137
Christian Greaves <gr34v0@gmail.com>
2017-05-10 15:37:27 -0400

    Commented both files, and made changes so code is PEP8 compliant. Changed collect_data() so that it only returns a single number. Want to keep most functionality out of the sensor driver. Changed main() so that it creates the pydict and THEN the JSON out of the data returned from collect_data() and then sends it along

--- SensorNode/main.py
+++ SensorNode/main.py
@@ -3,26 +3,40 @@ import FakeSensor
 import json
 import sys
 
+
 def main():
-    if( len(sys.argv) > 2 or len(sys.argv) < 2):
-        print "Parameters: <IP Address or URL to HOST server\n"
+    """ initializer of program. Will look for URL in argv, collect sensor data and then send data to said URL
+    :return: n/a
+    """
+
+    #  Check to see if the user supplied a commandline argument.
+    #  cmdline arg should be in form of a URL or Ip Address with specified Port.
+    if len(sys.argv) > 2 or len(sys.argv) < 2:
+        print "Parameters: <IP Address + Port number or URL to HOST server e.x. http://localhost:8080\n>"
         return
     else:
-        base_url = sys.argv[1]
+        #  User supplied cmdline arg, so we assume it's a URL...
+        url = sys.argv[1]
 
-    value = 0
+    index_value = 0
 
-    indexValue = 0
+    while True:
 
-    while(True):
-        value = FakeSensor.collect_data(indexValue)
+        #  Collecting data value from the sensor module/driver (in this case, FakeSensor
+        value = FakeSensor.collect_data(index_value)
 
-        response = requests.post(base_url, params=None, data=json.dumps(value, separators=(",",":")))
+        #  Packaging the data collected in a JSON object
+        data_parcel = json.dumps(({index_value: value}), separators=(",", ":"))
 
-        indexValue+=1
+        #  Calling post from the requests package to send our JSON data to the URL
+        response = requests.post(url, params=None, data=json.dumps(data_parcel, separators=(",", ":")))
 
-        print(response.text)  # TEXT/HTML
-        print(response.status_code, response.reason)  # HTTP
+        #  Incrementing index value so the next pass will provide a new key to the dict
+        index_value += 1
+
+        #  Printing various information about the POST requests to the terminal
+        print(response.text)
+        print(response.status_code, response.reason)
         print(response.raw.version)
 
 

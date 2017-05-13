3e2630d6a8e830c1b5f1c153e755fd1e5471f077
Adam Audycki <apa7395@rit.edu>
2017-05-12 22:48:33 -0400

    Added some more comments and fixed some of the spacing.

--- Host/NodeJS_WS/server.js
+++ Host/NodeJS_WS/server.js
@@ -1,6 +1,7 @@
 http = require('http');
 fs = require('fs');
 
+//Creates the connection to the db.
 var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -9,81 +10,79 @@ var connection = mysql.createConnection({
     database: 'cmpe'
 });
 
-
-server = http.createServer( function(req, res) {
+//Creates the actual webserver.
+server = http.createServer(function(req, res){
 
     console.dir(req.param);
 
 	//If a POST occurs.
-    if (req.method == 'POST') {
+    if (req.method == 'POST'){
         console.log("POST");
         var body = '';
-        req.on('data', function (data) {
+        req.on('data', function(data){
             body += data;
             console.log("Partial body: " + body);
         });
-        req.on('end', function () {
+        req.on('end', function(){
             console.log("Body: " + body);
-		writeToDB(body);
+			writeToDB(body);
         });
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end('post received');
     }
+	
     //If a GET occurs.
-    else
-    {
-        console.log("GET");
-        //var html = '<html><body><form method="post" action="http://localhost:3000">Name: <input type="text" name="name" /><input type="submit" value="Submit" /></form></body>';
-       var html = fs.readFileSync('index.html');
-	var db = getAllDB();
-	res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(html);
-	connection.query("SELECT * FROM data", function (err, rows, fields) {
-        if (!err) {
-		//rows = JSON.stringify(rows);
-		console.log(rows);
-		for (x in rows) {
-			res.write(""+rows[x]["data"]);
-			res.write("<br>");
-			
-		}
-	}
-        else {
-            console.log('Error while performing Query.');
-        }
-    });
+    else{
+		console.log("GET");
+		var html = fs.readFileSync('index.html');
+		var db = getAllDB();
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write(html);
+		connection.query("SELECT * FROM data", function (err, rows, fields){
+			if(!err){
+				//rows = JSON.stringify(rows);
+				console.log(rows);
+				for (x in rows){
+					res.write(""+rows[x]["data"]);
+					res.write("<br>");
+				}
+			}
+			else {
+				console.log('Error while performing Query.');
+			}
+		});
 
        //res.end();
-
-
-}
+	}
 });
 
 //The function will write the message into the database.
-function writeToDB(msg) {
+function writeToDB(msg){
     connection.query("INSERT INTO data (data) VALUES ('" + msg + "')", function (err, rows, fields) {
-        if (!err) {
+        if (!err){
             console.log('The solution is: ', rows);
         }
-        else {
+        else{
             console.log('Error while performing Query.');
         }
     });
 }
 
+//Just queries the db and returns all of the data.
 function getAllDB(){
     connection.query("SELECT * FROM data", function (err, rows, fields) {
-        if (!err) {
-		//rows = JSON.stringify(rows);
-		console.log(rows);
-		return rows;
+        if (!err){
+			//rows = JSON.stringify(rows);
+			console.log(rows);
+			return rows;
         }
-        else {
+        else{
             console.log('Error while performing Query.');
         }
     });
 }
 
+//Determines what port to use and what address to use.
 port = 8081;
 host = '192.168.43.250';
 server.listen(port, host);
